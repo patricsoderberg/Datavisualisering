e034b2339275a756c74b38e1719ca0c06c30ceeb
danhide <daniel.hidesten@hotmail.com>
2016-03-10 12:22:04 +0100

    Added content trying ajax request

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,5 +10,16 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', [function() {
-
+/*$(function () {
+    $.ajax({
+        type: "POST",
+url:"http://api.scb.se/OV0104/v1/doris/sv/ssd/START/SF/SF0101/ForaldraPenning",
+        data: '{"query":[{"code":"Region","selection":{"filter":"vs:RegionRiket99","values":["00"]}},{"code":"Kon","selection":{"filter":"item","values":["1","2"]}},{"code":"ContentsCode","selection":{"filter":"item","values":["SF0101AG"]}}],"response":{"format":"px"}}',
+        contentType: "application/json"
+    })
+    .done(function(res) {       
+    console.log('res', res)
+    });
+});*/
+    
 }]);
